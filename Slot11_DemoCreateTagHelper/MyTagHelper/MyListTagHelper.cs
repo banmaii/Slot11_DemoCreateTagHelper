3dd7f114900d2f11ecc7d1f80837b503fefbe325
banmaii <banmaibg201@gmail.com>
2023-02-20 06:05:03 +0700

    Add project files.

---
+++ Slot11_DemoCreateTagHelper/MyTagHelper/MyListTagHelper.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Slot11_DemoCreateTagHelper.MyTagHelper
+{
+    [HtmlTargetElement("mylist")]
+    public class MyListTagHelper : TagHelper
+    {
+        public string ListTitle { get; set; }
+        public List<String> ListItems { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ul";
+            output.TagMode = TagMode.StartTagAndEndTag;
+
+            output.Attributes.SetAttribute("class", "list-group");
+            output.PreElement.AppendHtml($"<h2>{ListTitle}</h2>");
+
+            StringBuilder content = new StringBuilder();
+            foreach(var item in ListItems)
+            {
+                content.Append($@"<li class=""list-group-item"">{item}</li>");
+            }
+            output.Content.SetHtmlContent(content.ToString());
+        }
+    }
+}
---
+++ Slot11_DemoCreateTagHelper/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+
+namespace Slot11_DemoCreateTagHelper.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [DisplayName("Ten nguoi dung")]
+
+        public string UserName { get; set; } = "Ban Mai";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
